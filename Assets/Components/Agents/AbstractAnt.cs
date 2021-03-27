164d5654dff1837d44241e480e4dc6bfeddbd280
KareemAbdelaty <69083495+KareemAbdelaty@users.noreply.github.com>
2021-03-27 03:17:58 +0200

    update to abstract ant

--- Assets/Components/Agents/AbstractAnt.cs
+++ Assets/Components/Agents/AbstractAnt.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class AbstractAnt : MonoBehaviour
 {
+    #region Fields
+
+    public int health;
+    public int health_share_chance;
+    public int queen_health_share_chance;
+    public int eatChance;
+    public int digChance;
+    public bool isqueen;
     // Start is called before the first frame update
     void Start()
     {
